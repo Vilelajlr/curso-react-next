5c2b076d727f4d5ef2ed4ca95f8480c0675bfb90
José Leandro <joseleandro2807@gmail.com>
2024-11-18 10:56:11 -0300

    Alteração de h1 para h2

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -5,7 +5,7 @@ export function PostCard({cover, title, body}){
     <div className='post'>
         <img src={cover} alt={title} />
         <div className='post-content'>
-          <h1>{title}</h1>
+          <h2>{title}</h2>
           <p>{body}</p>
         </div>
       </div>
